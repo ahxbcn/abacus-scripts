73ed970e61ddb2a47f2f22562e559be78d4928c5
ahxbcn <zhuhong850@gmail.com>
2026-04-08 14:23:52 +0800

    feat: read density matrix from file

--- cal_mayer_bond_order.py
+++ cal_mayer_bond_order.py
@@ -32,6 +32,35 @@ def read_overlap_matrix(ovlp_mat_file):
     
     return S
 
+def read_density_matrix(rho_mat_file):
+    """
+    Read density matrix from rho_mat_file.
+    """
+    with open(rho_mat_file, "r") as f:
+        lines = f.readlines()
+    
+    empty_lines_idx = [i for i, line in enumerate(lines) if not line.strip()]
+
+    print(empty_lines_idx)
+
+    dim_line = empty_lines_idx[-1] - 1
+    dims = [int(s) for s in lines[dim_line].split()]
+    
+    assert len(dims) == 2
+    assert dims[0] == dims[1]
+
+    ndim = dims[0]
+
+    vals = []
+    for iline in range(empty_lines_idx[-1]+1, len(lines)):
+        line_vals = [float(x) for x in lines[iline].split()]
+        vals.extend(line_vals)
+    
+    assert len(vals) == ndim * ndim
+    dm_mat = np.array(vals).reshape(ndim, ndim)
+
+    return dm_mat
+
 if __name__ == '__main__':
     abacusjob_dir = "./"
     print(f"Calculate Mayer bond order for {abacusjob_dir}")
@@ -41,3 +70,7 @@ if __name__ == '__main__':
     H2_ovlp_mat_file = f"{H2_abacusjob_dir}/OUT.ABACUS/data-0-S"
     H2_ovlp_mat = read_overlap_matrix(H2_ovlp_mat_file)
     print("H2 overlap matrix:", H2_ovlp_mat)
+
+    dm_file = f"{H2_abacusjob_dir}/OUT.ABACUS/SPIN1_DM"
+    dm = read_density_matrix(dm_file)
+    print("H2 density matrix:", dm)
