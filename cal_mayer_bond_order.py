26480f4c7a25e99c57f1dbe977769a86ee240527
ahxbcn <zhuhong850@gmail.com>
2026-04-08 14:02:30 +0800

    Initial script for cal_mayer_bond_order.py

---
+++ cal_mayer_bond_order.py
@@ -0,0 +1,9 @@
+"""
+Calculate Mayer bond order for gamma-only calculations of ABACUS using LCAO basis.
+"""
+from abacustest.lib_prepare.abacus import ReadInput
+
+if __name__ == '__main__':
+    abacusjob_dir = "./"
+    print(f"Calculate Mayer bond order for {abacusjob_dir}")
+    # TODO: finish implementation
